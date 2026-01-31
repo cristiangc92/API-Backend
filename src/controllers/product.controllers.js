f1988ce07a9c6a5599bad2c0e830b44aaa2b267f
Cristian Cacciolatti <cristiangc92@gmail.com>
2026-01-31 17:43:32 -0300

    feat: endpoint PUT /api/products/:id

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -58,3 +58,39 @@ export const createProduct = async (req, res) => {
         })        
     }
 }
+
+export const updateProduct = async (req, res) => {
+    const { id } = req.params
+    const { name, description, price, category, image } = req.body
+
+    if(!name || !price){
+        return res.status(400).json({
+            error: "El nombre y del precio son obligatorios"
+        })
+    }
+
+    try {
+        const result = await pool.query(
+            `UPDATE products
+            SET name = $1,
+            description = $2,
+            price = $3,
+            category = $4,
+            image = $5
+            WHERE id = $6
+            RETURNING *`, [name, description, price, category, image, id]
+        )
+
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                error: "Producto no encontrado"
+            })
+        }
+
+        res.json(result.rows[0])
+    } catch (error) {
+        res.status(500).json({
+            error: "Error al actualizar el producto"
+        })        
+    }
+}
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getAllProducts, getProductById, createProduct } from "../controllers/product.controllers.js";
+import { getAllProducts, getProductById, createProduct, updateProduct } from "../controllers/product.controllers.js";
 
 const router = Router()
 
 router.get("/products", getAllProducts)
 router.get("/products/:id", getProductById)
 router.post("/products", createProduct)
+router.put("/products/:id", updateProduct)
 
 export default router
