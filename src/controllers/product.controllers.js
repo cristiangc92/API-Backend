2ae631187947a89d1fcd4d3d2a34df66f4f897fc
Cristian Cacciolatti <cristiangc92@gmail.com>
2026-01-31 16:00:44 -0300

    feat: endpoint GET /api/products/:id

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -10,3 +10,25 @@ export const getAllProducts = async (req, res) => {
         res.status(500).json({ error: "Error al obtener productos" })
     }
 }
+
+export const getProductById = async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const result = await pool.query(
+            "SELECT * FROM products WHERE id = $1",[id] //$1 → query parametrizada (evita SQL injection)
+        )
+
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                error: "Producto no encontrado"
+            })
+        }
+
+        res.json(result.rows[0])
+    } catch (error) {
+        res.status(500).json({
+            error: "Error al obtener el producto"
+        })        
+    }
+}
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { getAllProducts } from "../controllers/product.controllers.js";
+import { getAllProducts, getProductById } from "../controllers/product.controllers.js";
 
 const router = Router()
 
 router.get("/products", getAllProducts)
+router.get("/products/:id", getProductById)
 
 export default router
